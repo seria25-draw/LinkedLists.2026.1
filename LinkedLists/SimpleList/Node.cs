9cfa57ab07811f07e78c335e61b7e835a411fee1
seria <seria@SERIANNY>
2026-05-05 21:05:57 -0500

    Add SimpleList project with generic Node<T> class
    Initialized solution and project files for SimpleList targeting .NET 10.0. Implemented a generic Node<T> class for linked lists with Data and Next properties. Note: Node constructor contains an assignment bug.

---
+++ LinkedLists/SimpleList/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleList;
+
+public class Node<T>
+    
+{
+    public Node(T Data)
+    {
+        Data = data;
+        Next = null;
+    }
+    public T? Data{ get; set; }
+
+    public Node<T>? Next { get; set; }
+}
